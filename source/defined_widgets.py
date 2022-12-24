98264725d2261d5afd6368a10b9795ad9a1e54c8
Evgenii-Barannik <radchemsu@gmail.com>
2022-12-25 01:41:40 +0200

    Widgets moved

--- source/defined_widgets.py
+++ source/defined_widgets.py
@@ -52,11 +52,7 @@ quantity_of_sample = widgets.Text(
     layout=dict(width=common_width),
 )
 
-comments = widgets.Text(
-    description="Comments",
-    style=dict(description_width=common_description_width),
-    layout=dict(width=common_width),
-)
+gap = widgets.Label("\xa0")
 
 submitting_company = widgets.Text(
     description="Submitting company",
@@ -81,7 +77,7 @@ phone_and_email_of_contact_person = widgets.Text(
     style=dict(description_width=common_description_width),
     layout=dict(width=common_width),
 )
-    
+
 sampler = widgets.Text(
     description="Sampler",
     style=dict(description_width=common_description_width),
@@ -113,7 +109,15 @@ results_sending_method = widgets.Dropdown(
     style=dict(description_width=common_description_width),
     layout=dict(width=common_width),
 )
-# Widgets for pdf printing
+
+comments = widgets.Text(
+    description="Comments",
+    style=dict(description_width=common_description_width),
+    layout=dict(width=common_width),
+)
+
+#### Widgets for pdf printing ####
+
 print_label = widgets.Label("Select the data to include in the PDF report:")
 
 print_average_alcohol_content_by_mass = widgets.Checkbox(
@@ -190,7 +194,21 @@ print_pdf_button = widgets.Button(
     tooltip='Create a PDF report with the selected data.',
 )
 
-# Lab widgets for Series 1
+#### Widgets for Series 1 ####
+
+was_first_step_of_decarbonisation_done_S1 = widgets.Dropdown(
+    description="Was the 1st step of decarbonisation done?",
+    options=["", True, False],
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+was_second_step_of_decarbonisation_done_S1 = widgets.Dropdown(
+    description="Was the 2nd step of decarbonisation done?",
+    options=["", True, False],
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
 
 mass_of_distillation_flask_empty_S1 = widgets.FloatText(
     description='Mass of distillation flask (empty), g',
@@ -206,13 +224,6 @@ mass_of_distillation_flask_with_sample_S1 = widgets.FloatText(
     layout=dict(width=common_width),
 )
 
-mass_of_distillation_flask_after_distillation_S1 = widgets.FloatText(
-    description='Mass of distillation flask (after distillation), g',
-    step=0.01,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
 was_50ml_of_water_added_to_distillation_flask_S1 = widgets.Dropdown(
     description="Was 50 ml of water added to distillation flask?",
     options=["", True, False],
@@ -222,13 +233,6 @@ was_50ml_of_water_added_to_distillation_flask_S1 = widgets.Dropdown(
 
 mass_of_receiver_flask_empty_S1 = widgets.FloatText(
     description='Mass of receiver flask (empty), g',
-    step=0.01,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
-mass_of_receiver_flask_after_distillation_S1 = widgets.FloatText(
-    description='Mass of receiver flask (after distillation), g',
     step=0.01,
     style=dict(description_width='initial'),
     layout=dict(width=common_width),
@@ -273,6 +277,29 @@ temperature_in_receiver_bath_S1 = widgets.IntSlider(
     max=25,
 )
 
+mass_of_receiver_flask_after_distillation_S1 = widgets.FloatText(
+    description='Mass of receiver flask (after distillation), g',
+    step=0.01,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+mass_of_distillation_flask_after_distillation_S1 = widgets.FloatText(
+    description='Mass of distillation flask (after distillation), g',
+    step=0.01,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+beer_pH_S1 = widgets.FloatText(
+    description='pH value of the decarbonized beer',
+    step=0.001,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+gap_S1 = widgets.Label("\xa0")
+
 pycnometer_number_S1 = widgets.FloatText(
     description="Pycnometer number (Series 1)",
     style=dict(description_width='initial'),
@@ -314,29 +341,24 @@ mass_of_pycnometer_with_residue_S1 = widgets.FloatText(
     layout=dict(width=common_width),
 )
 
-was_first_step_of_decarbonisation_done_S1 = widgets.Dropdown(
+visual_cue_line_S1 = widgets.Label("\xa0"*50 + "🍀☘️🌱 Series 1 🌱🍀☘️")  # "\xa0" is an empty space
+
+#### Widgets for Series 2 ###
+# Most these widgets are copies of widgets for Series 1, but with "_S1" -> "_S2" strings replaced.
+
+was_first_step_of_decarbonisation_done_S2 = widgets.Dropdown(
     description="Was the 1st step of decarbonisation done?",
     options=["", True, False],
     style=dict(description_width='initial'),
     layout=dict(width=common_width),
 )
 
-was_second_step_of_decarbonisation_done_S1 = widgets.Dropdown(
+was_second_step_of_decarbonisation_done_S2 = widgets.Dropdown(
     description="Was the 2nd step of decarbonisation done?",
     options=["", True, False],
     style=dict(description_width='initial'),
     layout=dict(width=common_width),
 )
-
-beer_pH_S1 = widgets.FloatText(
-    description='pH value of the decarbonized beer',
-    step=0.001,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
-# Lab widgets for Series 2
-# These widgets are copies of widgets for Series 1, but with "_S1" -> "_S2" strings replaced.
 
 mass_of_distillation_flask_empty_S2 = widgets.FloatText(
     description='Mass of distillation flask (empty), g',
@@ -352,13 +374,6 @@ mass_of_distillation_flask_with_sample_S2 = widgets.FloatText(
     layout=dict(width=common_width),
 )
 
-mass_of_distillation_flask_after_distillation_S2 = widgets.FloatText(
-    description='Mass of distillation flask (after distillation), g',
-    step=0.01,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
 was_50ml_of_water_added_to_distillation_flask_S2 = widgets.Dropdown(
     description="Was 50 ml of water added to distillation flask?",
     options=["", True, False],
@@ -368,13 +383,6 @@ was_50ml_of_water_added_to_distillation_flask_S2 = widgets.Dropdown(
 
 mass_of_receiver_flask_empty_S2 = widgets.FloatText(
     description='Mass of receiver flask (empty), g',
-    step=0.01,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
-mass_of_receiver_flask_after_distillation_S2 = widgets.FloatText(
-    description='Mass of receiver flask (after distillation), g',
     step=0.01,
     style=dict(description_width='initial'),
     layout=dict(width=common_width),
@@ -419,6 +427,29 @@ temperature_in_receiver_bath_S2 = widgets.IntSlider(
     max=25,
 )
 
+mass_of_receiver_flask_after_distillation_S2 = widgets.FloatText(
+    description='Mass of receiver flask (after distillation), g',
+    step=0.01,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+mass_of_distillation_flask_after_distillation_S2 = widgets.FloatText(
+    description='Mass of distillation flask (after distillation), g',
+    step=0.01,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+beer_pH_S2 = widgets.FloatText(
+    description='pH value of the decarbonized beer',
+    step=0.001,
+    style=dict(description_width='initial'),
+    layout=dict(width=common_width),
+)
+
+gap_S2 = widgets.Label("\xa0")
+
 pycnometer_number_S2 = widgets.FloatText(
     description="Pycnometer number (Series 2)",
     style=dict(description_width='initial'),
@@ -460,29 +491,10 @@ mass_of_pycnometer_with_residue_S2 = widgets.FloatText(
     layout=dict(width=common_width),
 )
 
-was_first_step_of_decarbonisation_done_S2 = widgets.Dropdown(
-    description="Was the 1st step of decarbonisation done?",
-    options=["", True, False],
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
+visual_cue_line_S2 = widgets.Label("\xa0"*50 + "🔮💎🧊 Series 2 🧊💎🔮")  # "\xa0" is an empty space
 
-was_second_step_of_decarbonisation_done_S2 = widgets.Dropdown(
-    description="Was the 2nd step of decarbonisation done?",
-    options=["", True, False],
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
-beer_pH_S2 = widgets.FloatText(
-    description='pH value of the decarbonized beer',
-    step=0.001,
-    style=dict(description_width='initial'),
-    layout=dict(width=common_width),
-)
-
-# Output widgets.
-# These are used to show results inside notebook.
+#### Output widgets ####
+# These widgets are used to display the results of calculations inside the notebook.
 
 tab1_output = widgets.Textarea(
     layout={'height': '100%', 'width': '95%'}
